ec3f81952c695b6e2c93773c1f6339c841e0847e
WillianAmorim <willian.amorim1@outlook.com>
2025-07-29 23:51:28 -0300

    adicionado pasta inicial de controllers, services e data, e também arquivo server.ts que é o nosso servidor da aplicação

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import * as http from 'http';
+
+const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {});
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
