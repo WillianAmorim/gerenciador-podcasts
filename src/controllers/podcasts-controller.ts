39855065e1b504d0da143598311f4066fa7e371b
Willian Amorim <willianamorim1@outlook.com.br>
2025-07-31 14:27:56 -0300

    adicionado enums no código para evitar strings soltas

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -3,17 +3,20 @@ import { serviceListEpisodes } from '../services/list-episodes-services';
 import { serviceFilterEpisodes } from "../services/filter-episodes-services";
 import { StatusCode } from "../utils/status-code";
 import { ContentType } from "../utils/content-type";
+import { FilterPodcastModel } from "../models/filter-podcast-model";
 
 export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
-    const content = await serviceListEpisodes();
+    const content: FilterPodcastModel = await serviceListEpisodes();
 
-    res.writeHead(StatusCode.OK, { 'Content-Type': ContentType.JSON });
-    res.end(JSON.stringify(content));
+    res.writeHead(content.statusCode, { 'Content-Type': ContentType.JSON });
+    res.write(JSON.stringify(content.body))
+    res.end();
 };
 
 export const getFilterEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
+    const content: FilterPodcastModel = await serviceFilterEpisodes(req.url);
 
-    const content = await serviceFilterEpisodes(req.url);
-    res.writeHead(StatusCode.OK, { 'Content-Type': ContentType.JSON });
-    res.end(JSON.stringify(content));
+    res.writeHead(content.statusCode, { 'Content-Type': ContentType.JSON });
+    res.write(JSON.stringify(content.body));
+    res.end();
 }
