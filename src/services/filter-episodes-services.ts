7913b2522f62645456f4d7e51934cdb7bd18252a
Willian Amorim <willianamorim1@outlook.com.br>
2025-07-31 10:07:09 -0300

    adicionado camada de lógica na service, para somente lá estar tratando a queryString e já chegar tudo certo no controller

--- src/services/filter-episodes-services.ts
+++ src/services/filter-episodes-services.ts
@@ -1,6 +1,10 @@
+import { IncomingMessage } from "http";
 import { repositoryPodcast } from "../repositories/podcast-repository";
 
-export const serviceFilterEpisodes = async (podcastName: string) => {
-    const data = await repositoryPodcast(podcastName);
-    return data.filter(episode => episode.podcastName === podcastName);
+export const serviceFilterEpisodes = async (podcastName: string | undefined) => {
+
+    const queryString = podcastName?.split('?p=')[1] || "";
+
+    const data = await repositoryPodcast(queryString);
+    return data;
 }
