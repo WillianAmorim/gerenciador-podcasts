19b675da7035640ad596ae8da260b2bfa9a83ef4
Willian Amorim <willianamorim1@outlook.com.br>
2025-07-31 10:23:00 -0300

    alterado status e content-type para enums criados

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,17 +1,19 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { serviceListEpisodes } from '../services/list-episodes-services';
 import { serviceFilterEpisodes } from "../services/filter-episodes-services";
+import { StatusCode } from "../utils/status-code";
+import { ContentType } from "../utils/content-type";
 
 export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
     const content = await serviceListEpisodes();
-    
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    res.writeHead(StatusCode.OK, { 'Content-Type': ContentType.JSON });
     res.end(JSON.stringify(content));
 };
 
 export const getFilterEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
 
     const content = await serviceFilterEpisodes(req.url);
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.writeHead(StatusCode.OK, { 'Content-Type': ContentType.JSON });
     res.end(JSON.stringify(content));
 }
