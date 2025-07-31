b341762e2f38238bdba6cdbb1480130c5ab419b8
WillianAmorim <willian.amorim1@outlook.com>
2025-07-31 00:41:10 -0300

    adicionado funções para listar todos os episódios e para filtrar por um episódio específico

---
+++ src/controllers/podcasts-controller.ts
@@ -0,0 +1,16 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { serviceListEpisodes } from '../services/list-episodes-services';
+import { serviceFilterEpisodes } from "../services/filter-episodes-services";
+
+export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
+    const content = await serviceListEpisodes();
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(content));
+};
+
+export const getFilterEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
+    const content = await serviceFilterEpisodes("flow");
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(content));
+}
