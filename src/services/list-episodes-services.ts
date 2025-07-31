8fa629cc4cdfad83ea4052eccd252c1b4008dc96
Willian Amorim <willianamorim1@outlook.com.br>
2025-07-31 14:29:58 -0300

    organizado função com o contrato de interface feito

--- src/services/list-episodes-services.ts
+++ src/services/list-episodes-services.ts
@@ -1,7 +1,20 @@
+import { FilterPodcastModel } from "../models/filter-podcast-model";
 import { repositoryPodcast } from "../repositories/podcast-repository";
+import { StatusCode } from "../utils/status-code";
 
-export const serviceListEpisodes = async () => {
+export const serviceListEpisodes = async (): Promise<FilterPodcastModel> => {
+
+    let responseFormat: FilterPodcastModel = {
+        statusCode: 0,
+        body: []
+    }
+
     const data = await repositoryPodcast();
 
-    return data;
+    responseFormat = {
+        statusCode: data.length !== 0 ? StatusCode.OK : StatusCode.NO_CONTENT,
+        body: data
+    }
+
+    return responseFormat;
 }
