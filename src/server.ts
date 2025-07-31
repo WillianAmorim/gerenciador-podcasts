a2a3c136ab24295f27906a37497fc15e648e370b
Willian Amorim <willianamorim1@outlook.com.br>
2025-07-31 10:23:30 -0300

    alterado metodos http em string para enums criados

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import * as http from 'http';
 import { getFilterEpisodes, getListEpisodes } from './controllers/podcasts-controller';
+import { Routes } from './routes/routes';
+import { HttpMethod } from './utils/http-methods';
 
 const server = http.createServer(async (req: http.IncomingMessage, res: http.ServerResponse) => {
     const [baseUrl, queryString] = req.url?.split('?') || [];
 
-    if(req.method === 'GET' && baseUrl === "/api/list") {
+    if(req.method === HttpMethod.GET && baseUrl === Routes.LIST) {
         await getListEpisodes(req, res);
     }
 
-    if(req.method === 'GET' && baseUrl?.startsWith("/api/episode")) {
+    if(req.method === HttpMethod.GET && baseUrl?.startsWith(Routes.EPISODES)) {
         await getFilterEpisodes(req, res);
     }
 });
