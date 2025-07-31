1cf130653cecaa419992bb2878bace42d2328318
Willian Amorim <willianamorim1@outlook.com.br>
2025-07-31 14:53:33 -0300

    retirado o query string e pego somente o baseUrs no indice 0 do array após a separação com split

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import { HttpMethod } from './utils/http-methods';
 import * as http from 'http';
 
 export const app = async (req: http.IncomingMessage, res: http.ServerResponse) => {
-    const [baseUrl, queryString] = req.url?.split('?') || [];
+    const baseUrl = req.url?.split('?')[0];
 
     if (req.method === HttpMethod.GET && baseUrl === Routes.LIST) {
         await getListEpisodes(req, res);
